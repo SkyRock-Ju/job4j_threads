10d714a1b6edccf4feea51300bc8b99c7e649b92
Dulustan <skyrockpk@gmail.com>
2022-10-08 17:40:01 +0700

    threads

---
+++ src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,28 @@
+package concurrent;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println(first.getState() + " first");
+        System.out.println(second.getState()+ " second");
+        first.start();
+        second.start();
+        while(!threadsAreTerminated(first, second)){
+            System.out.println(first.getState() + " first");
+            System.out.println(second.getState() + " second");
+        }
+        System.out.println(first.getState() + " first");
+        System.out.println(second.getState() + " second");
+    }
+
+    private static boolean threadsAreTerminated(Thread threadOne, Thread threadTwo) {
+        return threadOne.getState().equals(Thread.State.TERMINATED) &&
+                threadTwo.getState().equals(Thread.State.TERMINATED);
+    }
+}
