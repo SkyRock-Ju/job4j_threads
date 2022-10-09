e4ff1567e27f4cd467ba06884ba7479305ac2af9
Dulustan <skyrockpk@gmail.com>
2022-10-09 20:24:59 +0700

    Запускаю внутри потока

--- src/main/java/concurrent/Wget.java
+++ src/main/java/concurrent/Wget.java
@@ -5,14 +5,19 @@ public class Wget {
     public static void main (String [] args) {
 
         System.out.println("Start loading ... ");
-        for (int index = 0; index <= 100; index++){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.print("\rLoading : " + index  + "%");
-        }
+        Thread thread = new Thread(
+                () -> {
+                    for (int index = 0; index <= 100; index++){
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.print("\rLoading : " + index  + "%");
+                    }
+                }
+        );
+        thread.start();
         System.out.println();
         System.out.println("Loading completed.");
     }
