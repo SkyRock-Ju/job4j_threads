98e173df6cd772aa7c2ba99890f1dac81a3a714b
Dulustan <skyrockpk@gmail.com>
2022-10-11 21:20:04 +0700

    added Wget download feature

---
+++ src/main/java/thread/Wget.java
@@ -0,0 +1,48 @@
+package thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.security.InvalidParameterException;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[speed];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                Thread.sleep(1000);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        if (args[0].isEmpty()) {
+            throw new InvalidParameterException("URL is empty");
+        }
+        if (args[1].isEmpty()) {
+            throw new InvalidParameterException("speed value is empty");
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
+
+
