e6ebb2c29dd26d8fe8e78b761671e229d5f7795b
Dulustan <skyrockpk@gmail.com>
2022-10-24 19:20:32 +0700

    Node refactored

--- src/main/java/linked/Node.java
+++ src/main/java/linked/Node.java
@@ -2,31 +2,20 @@ package linked;
 
 import java.util.NoSuchElementException;
 
-public class Node<T> {
+public final class Node<T> {
     private final Node<T> next;
     private final T value;
 
-    private Node (Node<T> next, T value){
+    private Node(Node<T> next, T value) {
         this.next = next;
         this.value = value;
     }
 
-    public Node<T> getNext() throws NoSuchElementException{
-        if (next == null){
-            throw new NoSuchElementException("Next node doesn't exist");
-        }
+    public Node<T> getNext() throws NoSuchElementException {
         return this.next;
-    }
-
-    public Node<T> setNext(Node<T> next) {
-        return new Node<T>(next, value);
     }
 
     public T getValue() {
         return value;
-    }
-
-    public Node<T> setValue(T value) {
-        return new Node<>(next, value);
     }
 }
