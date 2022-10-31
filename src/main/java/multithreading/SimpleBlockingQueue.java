821f5c6026be0df35f967255a1ccf62d831192c0
Dulustan <skyrockpk@gmail.com>
2022-10-31 16:28:54 +0700

    added constructor with limit parameter to SimpleBlockingQueue.java

--- src/main/java/multithreading/SimpleBlockingQueue.java
+++ src/main/java/multithreading/SimpleBlockingQueue.java
@@ -11,7 +11,12 @@ public class SimpleBlockingQueue<T> {
 
     @GuardedBy("this")
     private final Queue<T> queue = new LinkedList<>();
-    private final int limit = 20;
+    private final int limit;
+
+    public SimpleBlockingQueue(int limit) {
+        this.limit = limit;
+    }
+
 
     public synchronized void offer(T value) throws InterruptedException {
         while (queue.size() == limit) {
