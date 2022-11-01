38f9cb8bb8c23ff49815bd60033c9e67d5ad2090
Dulustan <skyrockpk@gmail.com>
2022-11-01 19:07:00 +0700

    added ParallelSearch.java for lesson 3.1.4. Wait, Notify, NotifyAll

--- src/main/java/buffer/ParallelSearch.java
+++ src/main/java/buffer/ParallelSearch.java
@@ -5,10 +5,25 @@ import multithreading.SimpleBlockingQueue;
 public class ParallelSearch {
 
     public static void main(String[] args) {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+
+        final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+
         final Thread consumer = new Thread(
                 () -> {
-                    while (true) {
+                    while (producer.isAlive()) {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
@@ -20,18 +35,6 @@ public class ParallelSearch {
                 }
         );
         consumer.start();
-        new Thread(
-                () -> {
-                    for (int index = 0; index != 3; index++) {
-                        try {
-                            queue.offer(index);
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-
-        ).start();
+        producer.start();
     }
 }
