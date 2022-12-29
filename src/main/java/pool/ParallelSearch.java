54694a2fb7b2db931c58410c80f3964e2a8e6242
Dulustan <skyrockpk@gmail.com>
2022-12-29 15:01:42 +0700

    fixed ParallelSearch.java

--- src/main/java/pool/ParallelSearch.java
+++ src/main/java/pool/ParallelSearch.java
@@ -8,7 +8,7 @@ public class ParallelSearch<T> extends RecursiveTask<Integer> {
     private final int from;
     private final int to;
 
-    ParallelSearch(T[] array, T value, int from, int to) {
+    public ParallelSearch(T[] array, T value, int from, int to) {
         this.array = array;
         this.value = value;
         this.from = from;
@@ -17,13 +17,13 @@ public class ParallelSearch<T> extends RecursiveTask<Integer> {
 
     @Override
     protected Integer compute() {
-
         if (to - from <= 10) {
             return indexSearch(array, value, from, to);
         }
-        int mid = (from + to)/2;
-        ParallelSearch<T> leftSearch = new ParallelSearch<>(array, value, from, mid);
-        ParallelSearch<T> rightSearch = new ParallelSearch<>(array, value, mid, to);
+        int mid = (from + to) / 2;
+
+        ParallelSearch<Object> leftSearch = new ParallelSearch<>(array, value, from, mid);
+        ParallelSearch<Object> rightSearch = new ParallelSearch<>(array, value, mid, to);
 
         leftSearch.fork();
         rightSearch.fork();
@@ -31,15 +31,15 @@ public class ParallelSearch<T> extends RecursiveTask<Integer> {
         int left = leftSearch.join();
         int right = rightSearch.join();
 
-        if (left > right) {
-            return left;
-        } else if (right > left) {
-            return right;
+        if (left != right) {
+            return Math.max(left, right);
         }
         return -1;
     }
 
-    private int indexSearch(T[] array, T value, int from, int to) {
+
+    private static int indexSearch(Object[] array, Object value, int from, int to) {
+
         for (int i = from; i < to; i++) {
             if (array[i].equals(value)) {
                 return i;
